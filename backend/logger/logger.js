52786c5064a6f7ec7b899ed586634fa86474ddb4
Prashant Singh <prashant_singh@live.in>
2020-04-08 23:49:17 -0500

    add logger

---
+++ backend/logger/logger.js
@@ -0,0 +1,15 @@
+const Logger = function () {};
+
+Logger.prototype.info = function (logText) {
+  console.log(`[INFO]  ${new Date()} - ${logText}`);
+};
+
+Logger.prototype.debug = function (logText) {
+  console.log(`[DEBUG]  ${new Date()} - ${logText}`);
+};
+
+Logger.prototype.error = function (logText) {
+  console.log(`[ERROR]  ${new Date()} - ${logText}`);
+};
+
+module.exports = new Logger();
