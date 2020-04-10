4a649187be61fb181c35b17b5ea13170608caebc
Prashant Singh <prashant_singh@live.in>
2020-04-09 22:21:58 -0500

    backend: add user route file

---
+++ backend/routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const logger = require('../logger/logger');
+
+const middleware = require("../middlewares/middleware.js");
+
+const router = express.Router()
+
+module.exports = router;
