7878060ae4294351b69c061889b15163799913f6
Prashant Singh <prashant_singh@live.in>
2020-04-12 22:39:16 -0500

    backend: add code for user signup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,36 @@
 const express = require('express');
-const logger = require('../logger/logger');
+const bcrypt = require("bcrypt");
 
+const logger = require('../logger/logger');
 const middleware = require("../middlewares/middleware.js");
 
-const router = express.Router()
+const router = express.Router();
+
+const saltRounds = 10;
+
+router.post("/signup", async (req, res) => {
+    const { firstname, lastname, email, password } = req.body;
+    if (!(firstname || lastname || email || password)) {
+        errMsg = "mandatory field missing field in request";
+        logger.error(errMsg);
+        return res.status(401).json({ error: errMsg });
+    } 
+
+    try {
+        passwordHash = await bcrypt.hash(password, saltRounds)
+    } catch (err) {
+        errMsg = "mandatory field missing field in request";
+        logger.error(errMsg);
+        return res.status(500).json({ error: errMsg })
+    }
+
+    try {
+
+
+        // TODO: code for user login
+    } catch (err) {
+        // catch any error
+    } 
+});
 
 module.exports = router;
