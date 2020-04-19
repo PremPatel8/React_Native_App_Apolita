9cf4a2f8a4113c79fb05df561aa7923271549d92
Prashant Singh <prashant_singh@live.in>
2020-04-18 23:16:05 -0500

    code refactor

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 
 const logger = require('./logger/logger');
-const userRoutes = require("./routes/user")
+const userRoutes = require("./routes/user");
 
 const PORT = process.env.PORT || 8080;
 
