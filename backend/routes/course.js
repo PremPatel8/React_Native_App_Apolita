02fbdad0d33a49d7115dee45d95fc785b7b637ac
Prashant Singh <prashant_singh@live.in>
2020-04-21 23:30:46 -0500

    backend: route skeleton for enroll

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -5,6 +5,19 @@ const router = express.Router();
 router.post("/fetchall", async (req, res) => {
 }); 
 
+router.get("/enroll", async (req, res) => {
+    const { courseID, userID } = req.body;
+    if (!(courseID || userID)) {
+        errMsg = "mandatory field missing field in request";
+        logger.error(errMsg);
+        return res.status(401).json({ error: errMsg });
+    }
 
+    try {
+        // TODO: code to enroll the user 
+    } catch {
+        // catch any error
+    }
+}); 
 
 module.exports = router;
