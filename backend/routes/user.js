09f6ad2c0c3820858a68c4ec77fdc3fb9b6044e4
Prashant Singh <prashant_singh@live.in>
2020-04-21 23:28:35 -0500

    backend: bugfixes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -34,7 +34,7 @@ router.post("/signup", async (req, res) => {
 
 router.post("/login", async (req, res) => {
     const { email, password } = req.body;
-    if (!(firstname || lastname || email || password)) {
+    if (!(email || password)) {
         errMsg = "mandatory field missing field in request";
         logger.error(errMsg);
         return res.status(401).json({ error: errMsg });
