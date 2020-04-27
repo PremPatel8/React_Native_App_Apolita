0ac50b7aaa9c94fb21d99361e053d45e57785fbc
Prashant Singh <prashant_singh@live.in>
2020-04-26 22:41:22 -0500

    add code for user creation

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bcrypt = require("bcrypt");
 
+// const middleware = require("../middlewares/middleware.js");
 const logger = require('../logger/logger');
-const middleware = require("../middlewares/middleware.js");
+const User = require('../models/user.js');
 
 const router = express.Router();
 const saltRounds = 10;
@@ -14,8 +15,7 @@ router.post("/signup", async (req, res) => {
         return res.status(400).json({ error: errMsg });
     }
 
-    const { firstname, lastname, email, password } = req.body;
-    if (!(firstname || lastname || email || password)) {
+    if (!(req.body.firstname || req.body.lastname || req.body.email || req.body.password || req.body.gender || req.body.phonenumber || req.body.city || req.body.country)) {
         errMsg = "mandatory field missing field in request";
         logger.error(errMsg);
         return res.status(401).json({ error: errMsg });
@@ -23,6 +23,27 @@ router.post("/signup", async (req, res) => {
 
     try {
         passwordHash = await bcrypt.hash(password, saltRounds)
+
+        const user = new User({
+            firstname = req.body.firstname,
+            lastname = req.body.lastname,
+            email = req.body.email,
+            password = req.body.password,
+            gender = req.body.gender,
+            phonenumber = req.body.phonenumber,
+            city = req.body.city,
+            country = req.body.country,
+        });
+
+        User.create(user, (err, data) => {
+            if (err) {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while creating the Customer."
+                });
+            } 
+            
+            res.send(data);
+          });
     } catch (err) {
         errMsg = "password encryption failed, err: " + err;
         logger.error(errMsg);
