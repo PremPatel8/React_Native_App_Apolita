235351f76adc726f3ea3df45305ecea6488575df
meghavijendra <megha27996@gmail.com>
2020-04-28 15:14:58 -0500

    DB calls added to frontend

--- frontend/screens/LoginScreen.js
+++ frontend/screens/LoginScreen.js
@@ -1,43 +1,116 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import { Header, Divider } from 'react-native-elements';
+import Typography from '@material-ui/core/Typography';
+
 export default class LoginScreen extends React.Component {
-  state={
-    email:"",
-    password:""
-  }
-  render(){
+  state = {
+    email: '',
+    password: '',
+    errorMessage: '',
+  };
+
+  handleSubmit = async (evt) => {
+    evt.preventDefault();
+
+    const reqUrl = `URLL`;
+    try {
+      const response = await fetch(reqUrl, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: evt.target.email.value,
+          password: evt.target.password.value,
+        }),
+      });
+      if (response.status === 200) {
+        this.setState({ errorMessage: '' });
+        this.props.navigation.navigate('Dashboard');
+      } else if (response.status === 401) {
+        this.setState({ errorMessage: 'Invalid Email and/or Password' });
+      } else {
+        this.setState({
+          errorMessage:
+            'There was some error while processing your request. If this contiunes please contact support. ',
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  render() {
+    const { errorMessage } = this.state;
     return (
       <View style={styles.container}>
-        <Header style={{ position:'absolute'}}
-            centerComponent={{ text: 'Welcome to Apolita', style: { color: '#fff', fontSize: 25  } }}
-            backgroundColor='#00BFFF'
+        <Header
+          style={{ position: 'absolute' }}
+          centerComponent={{
+            text: 'Welcome to Apolita',
+            style: { color: '#fff', fontSize: 25 },
+          }}
+          backgroundColor='#00BFFF'
         />
-        <Text style={styles.logo}></Text> 
-        <View style={styles.inputView} >
-          <TextInput  
+        <Text style={styles.logo}></Text>
+        <View style={styles.inputView}>
+          <TextInput
             style={styles.inputText}
-            placeholder="Email..." 
-            placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({email:text})}/>
+            placeholder='Email...'
+            name='email'
+            placeholderTextColor='#003f5c'
+            onChangeText={(text) => this.setState({ email: text })}
+          />
         </View>
-        <View style={styles.inputView} >
-          <TextInput  
+        <View style={styles.inputView}>
+          <TextInput
             secureTextEntry
             style={styles.inputText}
-            placeholder="Password..." 
-            placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({password:text})}/>
+            name='password'
+            placeholder='Password...'
+            placeholderTextColor='#003f5c'
+            onChangeText={(text) => this.setState({ password: text })}
+          />
         </View>
-        <TouchableOpacity style={styles.loginBtn} onPress={() => this.props.navigation.navigate('Dashboard')}>
+        {errorMessage && (
+          <Typography
+            variant='caption'
+            component='p'
+            style={{ color: 'red', padding: 10 }}
+          >
+            {errorMessage}
+          </Typography>
+        )}
+        <TouchableOpacity
+          style={styles.loginBtn}
+          onPress={() => this.handleSubmit}
+        >
           <Text style={styles.loginText}>LOGIN</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('PasswordReset')}>
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate('PasswordReset')}
+        >
           <Text style={styles.forgot}>Forgot Password?</Text>
         </TouchableOpacity>
-        <View style={{ padding: 20}}></View>
-        <Divider style={{ backgroundColor: '#2471A3', height: 1, width: 290 }}/>
-        <TouchableOpacity style={styles.signupBtn} onPress={() => this.props.navigation.navigate('SignUp')}>
+        <View style={{ padding: 20 }}></View>
+        <Divider
+          style={{
+            backgroundColor: '#2471A3',
+            height: 1,
+            width: 290,
+          }}
+        />
+        <TouchableOpacity
+          style={styles.signupBtn}
+          onPress={() => this.props.navigation.navigate('SignUp')}
+        >
           <Text style={styles.loginText}>SignUp</Text>
         </TouchableOpacity>
       </View>
@@ -50,58 +123,58 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-//    justifyContent: 'center',
+    //    justifyContent: 'center',
   },
-  logo:{
-//    fontWeight:"bold",
-    fontSize:40,
-    color:"#2471A3",
-    marginBottom:40,
+  logo: {
+    //    fontWeight:"bold",
+    fontSize: 40,
+    color: '#2471A3',
+    marginBottom: 40,
     textAlignVertical: 'top',
     justifyContent: 'flex-start',
   },
-  inputView:{
-    width:"80%",
+  inputView: {
+    width: '80%',
     backgroundColor: '#fff',
-    height:50,
-    marginBottom:40,
+    height: 50,
+    marginBottom: 40,
     borderBottomColor: '#2471A3',
     borderBottomWidth: 2,
-    padding:1
+    padding: 1,
   },
-  inputText:{
-    height:50,
-    fontSize:15,
-    color:"#003f5c"
+  inputText: {
+    height: 50,
+    fontSize: 15,
+    color: '#003f5c',
   },
-  forgot:{
-    color:"#2471A3",
-    fontSize:14
+  forgot: {
+    color: '#2471A3',
+    fontSize: 14,
   },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#00BFFF",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
+  loginBtn: {
+    width: '80%',
+    backgroundColor: '#00BFFF',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
     width: 210,
-    marginBottom:20
+    marginBottom: 20,
   },
-  signupBtn:{
-    width:"80%",
-    backgroundColor:"#00BFFF",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
+  signupBtn: {
+    width: '80%',
+    backgroundColor: '#00BFFF',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
     width: 210,
-    marginBottom:10
+    marginBottom: 10,
   },
-  loginText:{
-    color:"white",
-    fontSize: 20
-  }
+  loginText: {
+    color: 'white',
+    fontSize: 20,
+  },
 });
