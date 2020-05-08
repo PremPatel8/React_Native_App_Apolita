8c01e5c5a8fde23ba08c6c5175f6bc0e515136f7
Prem Patel <prempatel_8@outlook.com>
2020-05-08 18:44:02 -0500

    Update course.js
    Added the API endpoint to retrieve a list of file names and paths related to a particular course ID

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const fs = require("fs")
+
+var path = require('path');
+
 router.post("/fetchall", async (req, res) => {
 }); 
 
@@ -20,4 +24,35 @@ router.get("/enroll", async (req, res) => {
     }
 }); 
 
+router.get("/api/course/:id", async(req, res) => {
+    const courseID = req.params.id
+    
+    // console.log(courseID)
+    
+    //change public to the name of the directory where all the assessts are stored on your system
+    var folderPath = path.join(__dirname,'..', 'public', courseID)
+    
+    // console.log(folderPath)
+    try {
+        const arrayOfFiles = getAllFiles(folderPath)
+        // console.log(arrayOfFiles)
+        res.send(arrayOfFiles)
+      } catch(e) {
+        console.log(e)
+      } 
+});
+
+const getAllFiles = function(dirPath, arrayOfFiles) {
+    files = fs.readdirSync(dirPath)
+    arrayOfFiles = arrayOfFiles || []
+    files.forEach(function(file) {
+      if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+        arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+      } else {
+        arrayOfFiles.push(path.join(dirPath, "/", file))
+      }
+    })
+    return arrayOfFiles
+  }
+ 
 module.exports = router;
