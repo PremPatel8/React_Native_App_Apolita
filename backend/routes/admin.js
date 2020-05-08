482434044d5c4db4808e02a3e01000f4e3fb9c16
Prashant Singh <prashant_singh@live.in>
2020-05-08 00:08:23 -0500

    backend: add 'addcourse' route for admin

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const logger = require('../logger/logger');
+const Course = require('../models/admin');
+
 const router = express.Router();
 
 router.post("/addcourse", async (req, res) => {
@@ -16,9 +19,34 @@ router.post("/addcourse", async (req, res) => {
     }
 
     try {
+        const course = new Course({
+            name: req.body.name,
+            description: req.body.description,
+            image: req.body.image,
+        });
 
+        Course.create(course, (err, data) => {
+            if (err) {
+                logger.error(err.message);
+                res.status(500).json({
+                    message: err.message || "encountered error while creating the course."
+                });
+            }
+
+            return res.status(200).json(data);
+        });
     } catch {
-        
+        errMsg = "encountered error while creating the course: " + err;
+        logger.error(errMsg);
+        return res.status(500).json({ error: errMsg });
+    }
+});
+
+router.post("/fetchallcourse", async (req, res) => {
+    if (!req.body) {
+        errMsg = "request body cannot be empty for POST route: /admin/addcourse";
+        logger.error(errMsg);
+        return res.status(400).json({ error: errMsg });
     }
 
 });
