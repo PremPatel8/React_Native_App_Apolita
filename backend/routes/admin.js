df8791b5d83758a47ab612bd69eebaf8e1f29ec2
Prashant Singh <prashant_singh@live.in>
2020-05-07 23:29:47 -0500

    backend: add create course api for admin

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -2,22 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post("/fetchall", async (req, res) => {
-}); 
+router.post("/addcourse", async (req, res) => {
+    if (!req.body) {
+        errMsg = "request body cannot be empty for POST route: /admin/addcourse";
+        logger.error(errMsg);
+        return res.status(400).json({ error: errMsg });
+    }
 
-router.get("/enroll", async (req, res) => {
-    const { courseID, userID } = req.body;
-    if (!(courseID || userID)) {
+    if (!(req.body.name || req.body.description || req.body.image)) {
         errMsg = "mandatory field missing field in request";
         logger.error(errMsg);
         return res.status(401).json({ error: errMsg });
     }
 
     try {
-        // TODO: code to enroll the user 
+
     } catch {
-        // catch any error
+        
     }
-}); 
+
+});
 
 module.exports = router;
