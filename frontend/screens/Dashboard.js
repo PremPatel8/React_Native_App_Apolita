dcb0791b965909f69ad52c8dbb5e0c02e9da7a14
Rahul Tevatia <39152497+rtevatia4@users.noreply.github.com>
2020-05-09 18:20:42 -0500

    Update Dashboard.js

--- frontend/screens/Dashboard.js
+++ frontend/screens/Dashboard.js
@@ -5,14 +5,62 @@ import { DrawerActions } from 'react-navigation-drawer';
 import { FontAwesome } from '@expo/vector-icons';
 
 export default class Dashboard extends React.Component {
-//  toggleDrawer = () => {
-//    //Props to open/close the drawer
-//    this.props.navigationProps.toggleDrawer();
-//  };
-//  state={
-//    email:"",
-//    password:""
-//  }
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      name: this.props.navigation.state.params.name,
+      email: this.props.navigation.state.params.email,
+      phone: this.props.navigation.state.params.phone,
+      city: this.props.navigation.state.params.city,
+      errorMessage: '',
+    };
+  }
+  componentDidMount = async () => {
+    const response = await fetch(`http://61a88a7c.ngrok.io/course/fetchall`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+      }),
+    });
+    console.log('inside mounter')
+    if (response.status === 200) {
+      response.json().then(data => {
+      this.setState({ errorMessage: '' });
+      const result = Object.keys(data).map(key => ({[key]: data[key]}));
+      console.log(result)
+      {/*data.forEach(element => {
+        <Card
+            title={element.name}
+            image={require('../assets/icon.png')}>
+            <Text style={{marginBottom: 10}}>
+                    {element.description}
+            </Text>
+            <Button
+                icon={<Icon name='code' color='#ffffff' />}
+                buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 10}}
+                title='Launch Course' />
+            <TouchableOpacity>
+                <Text style={{ fontSize: 15, color: 'blue' }}>Take Assesment</Text>
+            </TouchableOpacity>
+        </Card>
+        });*/}
+      });
+//      this.props.navigation.navigate('PassResetSuccess')
+    } else if (response.status === 401) {
+      this.setState({ errorMessage: 'Please enter correct email id.' });
+    } else if (response.status === 402) {
+      this.setState({ errorMessage: 'Details Missing' });
+    } else {
+      this.setState({
+        errorMessage:
+          'There was some error while processing your request. If this continues please contact support. ',
+      });
+    }
+  };
   render(){
     return (
       <View style={styles.container}>
@@ -20,11 +68,12 @@ export default class Dashboard extends React.Component {
             leftComponent={< Icon name='menu' color='#fff' onPress={() => this.props.navigation.dispatch(DrawerActions.openDrawer())}
             />}
             centerComponent={{ text: 'Courses Dashboard', style: { color: '#fff', fontSize: 20 } }}
-            rightComponent={ <FontAwesome name='user-circle' color='#fff' size={21} onPress={() => this.props.navigation.navigate('UserProfile')}
+            rightComponent={ <FontAwesome name='user-circle' color='#fff' size={21} onPress={() => this.props.navigation.navigate('UserProfile', {name: this.state.name, email: this.state.email, phone: this.state.phone, city: this.state.city})}
             />}
             backgroundColor='#00BFFF'
           />
         <ScrollView>
+        {/*  {this.handleSubmit()}
         <Card
             title='Classroom Course 1'
             image={require('../assets/icon.png')}>
@@ -66,7 +115,7 @@ export default class Dashboard extends React.Component {
             <TouchableOpacity>
                 <Text style={{ fontSize: 15, color: 'blue' }}>Take Assesment</Text>
             </TouchableOpacity>
-        </Card>
+        </Card>*/}
         </ScrollView>
     </View>
     );
