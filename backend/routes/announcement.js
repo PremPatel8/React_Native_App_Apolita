731d5e564643d695ae641b056fe9b98bf792e4ac
Prashant Singh <prashant_singh@live.in>
2020-05-10 01:16:37 -0500

    backend: add fetch announcement by id

--- backend/routes/announcement.js
+++ backend/routes/announcement.js
@@ -5,6 +5,28 @@ const Announcemnt = require('../models/announcement');
 
 const router = express.Router();
 
+router.get("/:announcementid", async (req, res) => {
+    try {
+        Announcemnt.findByAnnouncementID(announcementid, (err, data) => {
+            if ( err ) {
+                if (err.kind === "not_found") {
+                    errMsg = `no announcement found with courseid: ${announcementid}`;
+                    logger.error(errMsg);
+                    return res.status(404).json({ error: errMsg });
+    ]           } else {
+                    errMsg = `encountered error while fetching announcement with announcementid: ${announcementid}`;
+                    logger.error(errMsg);
+                    return res.status(500).json({ error: errMsg });
+                }
+            } else return res.status(200).json(data);
+        });
+    } catch (err) {
+        errMsg = "encountered error while fetching the courses: " + err;
+        logger.error(errMsg);
+        return res.status(500).json({ error: errMsg });
+    }
+});
+
 router.get("/fetchall", async (req, res) => {
     try {
         Announcemnt.fetchAll((err, data) => {
