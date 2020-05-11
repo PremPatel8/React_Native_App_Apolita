82487e004b90965bb48a4da286a30f9f37c1b790
Rahul Tevatia <39152497+rtevatia4@users.noreply.github.com>
2020-05-10 22:49:41 -0500

    Add files via upload

---
+++ frontend/screens/AddAnnouncement.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import { Header, Icon } from 'react-native-elements';
+import TextField from '../components/TextField';
+
+export default class AddAnnouncement extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            description: '',
+            errorMessage: '',
+        };
+    }
+  handleSubmit = async () => {
+    const response = await fetch(`http://9466b7f3.ngrok.io/admin/addannouncement`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title: this.state.title,
+        description: this.state.description,
+      }),
+    });
+    if (response.status === 200) {
+      this.setState({ errorMessage: ' Announcement sent to the students',
+                 title: '',
+                 description: ''});
+//      this.props.navigation.navigate('PassResetSuccess')
+    } else if (response.status === 401) {
+      this.setState({ errorMessage: 'Please enter Announcement details to send.' });
+    } else if (response.status === 402) {
+      this.setState({ errorMessage: 'Details Missing' });
+    } else {
+      this.setState({
+        errorMessage:
+          'There was some error while processing your request. If this continues please contact support. ',
+      });
+    }
+  };
+  render() {
+    const { errorMessage } = this.state;
+    return (
+      <View style={styles.container}>
+          <Header style={{ position:'absolute'}}
+            leftComponent={< Icon name='arrow-back' color='#fff' onPress={() => this.props.navigation.goBack(null)}/>}
+            centerComponent={{ text: 'Add an announcement', style: { color: '#fff', fontSize: 18  } }}
+            backgroundColor='#00BFFF'
+          />
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>Please provide Announcement details:</Text>
+              <TextField style={{  }} value={this.state.title} onChangeText={(title) => this.setState({title})} placeHolder=" Announcement Title"/>
+              <TextField style={{  }} value={this.state.description} onChangeText={(description) => this.setState({description})} placeHolder=" Announcement Description"/>
+              <Text style={{ color: 'red' }}>{errorMessage}</Text>
+              <TouchableOpacity style={styles.buttonContainer} onPress={() => this.handleSubmit()}>
+                <Text>Tap to send announcement</Text>  
+              </TouchableOpacity>              
+            </View>
+           </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  body:{
+    marginTop:0,
+  },
+  bodyContent: {
+//    flex: 1,
+    alignItems: 'center',
+    padding:30,
+  },
+  name:{
+    fontSize:17,
+    color: "#696969",
+    fontWeight: "600",
+    marginBottom: 30
+  },
+  buttonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#00BFFF",
+  },
+});
---
+++ frontend/screens/AdminDashboard.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { StyleSheet, Text, Image, ScrollView, View, TouchableOpacity } from 'react-native';
+import { Header } from 'react-native-elements';
+import { FontAwesome } from '@expo/vector-icons';
+import DetailListItem from '../components/DetailListItem';
+
+export default class AdminDashboard extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      name: this.props.navigation.state.params.name,
+      email: this.props.navigation.state.params.email,
+      phone: this.props.navigation.state.params.phone,
+      city: this.props.navigation.state.params.city,
+      errorMessage: '',
+      data: [],
+      isLoading: true,
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+          <Header style={{ position:'absolute'}}
+            centerComponent={{ text: 'Instructor Dashboard', style: { color: '#fff', fontSize: 20 } }}
+            rightComponent={ <FontAwesome name='user-circle' color='#fff' size={21} onPress={() => this.props.navigation.navigate('UserProfile', {name: this.state.name, email: this.state.email, phone: this.state.phone, city: this.state.city})}
+            />}
+            backgroundColor='#00BFFF'
+          />
+        <ScrollView>
+        <View style={styles.bodyContent}>
+            <TouchableOpacity onPress={() => this.props.navigation.navigate('StudentsEnrolled')}>
+              <DetailListItem
+                  icon="account-circle"
+                  title="Students Enrolled"
+                  subtitle="Tap to check total students enrolled."
+              /></TouchableOpacity>
+              <TouchableOpacity>
+              <DetailListItem
+                  icon="airplay"
+                  title="Add course"
+                  subtitle="Tap to add new course."
+              /></TouchableOpacity>
+              <TouchableOpacity onPress={() => this.props.navigation.navigate('AddAnnouncement')}>
+              <DetailListItem
+                  icon="announcement"
+                  title="Add Announcement"
+                  subtitle="Tap to add new announcement"
+              /></TouchableOpacity>
+            </View>
+            <TouchableOpacity
+                style={styles.logoutBtn}
+                onPress={() => this.props.navigation.navigate('Home')}>
+                <Text style={styles.loginText}>Sign off</Text>
+            </TouchableOpacity>
+        </ScrollView>
+    </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+//    paddingTop: 10,
+    backgroundColor: '#fff',
+//    alignItems: 'center',
+ //   justifyContent: 'center',
+  },
+  scrollView: {
+    flex: 1,
+  },
+  logo:{
+//    fontWeight:"bold",
+    fontSize:30,
+    color:"#2471A3",
+    marginBottom:40,
+    textAlignVertical: 'top',
+    justifyContent: 'flex-start',
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"white",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  bodyContent: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  inputText:{
+    height:50,
+    color:"#003f5c"
+  },
+  forgot:{
+    color:"white",
+    fontSize:14
+  },
+  logoutBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginLeft: 40,
+    marginTop:150,
+    marginBottom:10
+  },
+  signupBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  }
+});
